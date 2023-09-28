52fbf899176096da3eb4ed5eee5cd735e1883cbf
Freeze2112 <111502931+Freeze2112@users.noreply.github.com>
2023-09-28 16:37:24 +0630

    NavBar

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:rafflix/utils/nav.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const NavBar();
+  }
+}
