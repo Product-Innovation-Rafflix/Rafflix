cde39955519e607348d8f01950f8bd629ca2bca2
Aung Myat Min <koaungmyatmin0@gmail.com>
2023-09-28 23:55:01 +0630

    Added Flex box to the main widget

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:rafflix/utils/nav.dart';
+import 'package:rafflix/utils/flexBox.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const NavBar();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize:
+            const Size.fromHeight(100.0), // Define the preferred height
+        child: NavBar(), // Your NavBar widget
+      ),
+      body: FlexBox(), // Your content goes here
+    );
   }
 }
