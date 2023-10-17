60a13e3d13a601cdeaa7a79af8f771127b058ca5
Xem <phyominkhant.xem@gmail.com>
2023-10-17 20:47:59 +0630

    itemdetails

---
+++ lib/models/database.dart
@@ -0,0 +1,64 @@
+class CustomData {
+  static List<Map> mydata = [
+    {
+      "id": 1,
+      "name": 'Tester1',
+    },
+    {
+      "id": 2,
+      "name": 'Tester2',
+    },
+    {
+      "id": 3,
+      "name": 'Tester3',
+    },
+    {
+      "id": 4,
+      "name": 'Tester4',
+    },
+    {
+      "id": 5,
+      "name": 'Tester5',
+    },
+    {
+      "id": 6,
+      "name": 'Tester6',
+    },
+    {
+      "id": 7,
+      "name": 'Tester7',
+    },
+    {
+      "id": 8,
+      "name": 'Tester8',
+    },
+    {
+      "id": 9,
+      "name": 'Tester9',
+    },
+    {
+      "id": 10,
+      "name": 'Tester10',
+    },
+    {
+      "id": 11,
+      "name": 'Tester11',
+    },
+    {
+      "id": 12,
+      "name": 'Tester12',
+    },
+    {
+      "id": 13,
+      "name": 'Tester13',
+    },
+    {
+      "id": 14,
+      "name": 'Tester14',
+    },
+    {
+      "id": 15,
+      "name": 'Tester15',
+    },
+  ];
+}
--- lib/pages/ItemDetails.dart
+++ lib/pages/ItemDetails.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ItemDetails extends StatefulWidget {
-  const ItemDetails({super.key});
+  const ItemDetails({Key? key}) : super(key: key);
 
   @override
   State<ItemDetails> createState() => _ItemDetailsState();
 }
 
 class _ItemDetailsState extends State<ItemDetails> {
+  List<Map> mydata = [
+    {
+      "id": 1,
+      "name": '1',
+    },
+    {
+      "id": 2,
+      "name": '2',
+    },
+    {
+      "id": 3,
+      "name": '3',
+    },
+  ];
+
+  List<Map> selectedItems = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,42 +48,147 @@ class _ItemDetailsState extends State<ItemDetails> {
               ),
             ),
             Positioned(
-                left: 20,
-                top: 70,
-                child: Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.arrow_back_rounded))
-                  ],
-                )),
+              left: 20,
+              top: 70,
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.arrow_back_rounded),
+                  ),
+                ],
+              ),
+            ),
             Positioned(
-                top: 300,
-                child: Container(
-                  padding: const EdgeInsets.only(left: 20, top: 30),
-                  width: MediaQuery.of(context).size.width,
-                  height: 550,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
-                      )),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("iPhone 15 Pro Max",
+              top: 300,
+              child: Container(
+                padding: const EdgeInsets.only(left: 20, top: 30, right: 20),
+                width: MediaQuery.of(context).size.width,
+                height: 550,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "iPhone 15 Pro Max",
                           style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w800)),
-                      Text(
-                        "5000 Ks / Tickets",
-                        style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.w800),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "5000 Ks / Tickets",
+                          style: TextStyle(
                             fontSize: 20,
                             fontWeight: FontWeight.w800,
-                            color: Colors.black54),
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Select Your Ticket",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w800),
+                        ),
+                        Text(
+                          "49 Ticket Left",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w800),
+                        ),
+                      ],
+                    ),
+                    GridView.count(
+                      crossAxisCount: 7,
+                      crossAxisSpacing: 5.0,
+                      mainAxisSpacing: 5.0,
+                      shrinkWrap: true,
+                      children: List.generate(
+                        mydata.length,
+                        (index) {
+                          return ItemCard(
+                            onTap: () {
+                              setState(() {
+                                if (selectedItems.contains(mydata[index])) {
+                                  selectedItems.remove(mydata[index]);
+                                } else {
+                                  selectedItems.add(mydata[index]);
+                                }
+                              });
+                            },
+                            item: mydata[index],
+                            isSelected: selectedItems.contains(mydata[index]),
+                          );
+                        },
                       ),
-                    ],
-                  ),
-                ))
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Description",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  final Map item;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  ItemCard({
+    required this.item,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        color: isSelected ? Colors.blue : Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("${item['name']}"),
+            // Add additional fields as needed
           ],
         ),
       ),
